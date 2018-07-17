2ab17f4080d691636255acc3ab17fcfbf27d87da
justinchristensen66 <justin.christensen0@gmail.com>
2018-07-17 14:48:20 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/layout/App.js";
+import registerServiceWorker from "./registerServiceWorker";
+
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App.js", () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
+registerServiceWorker();
