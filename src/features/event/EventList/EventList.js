60075d34944d68dff0a2829c1385f1e8556d8931
justinchristensen66 <justin.christensen0@gmail.com>
2018-07-25 11:23:20 -0700

    finished static event dashboard section 1

---
+++ src/features/event/EventList/EventList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import EventListItem from "./EventListItem";
+
+export class EventList extends Component {
+  render() {
+    const {events} =this.props;
+    return (
+      <div>
+        <h1>Events List</h1>
+        {events.map((event) => (
+          <EventListItem key={event.id} event={event} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default EventList;
